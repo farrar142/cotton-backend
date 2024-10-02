db188b7f3c0970b191a4d33d7f2c3752f64b42a2
farrar142 <gksdjf1690@gmail.com>
2024-10-02 17:52:01 +0900

    fix:팔로우 모델이 생성될때의 알람 액션 수정

--- relations/signals.py
+++ relations/signals.py
@@ -1,15 +1,41 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
 
-from .models import Follow
+from .models import Follow, User
 
 
-@receiver(post_save, sender=Follow)
-def on_following_created(sender, instance: Follow, **kwargs):
+@receiver(m2m_changed, sender=Follow)
+def on_following_created(sender, instance: User, **kwargs):
     from notifications.models import Notification
 
-    noti = Notification()
-    noti.user = instance.following_to
-    noti.from_user = instance.followed_by
-    noti.followed_user = instance
-    noti.save()
+    pk_set: set[int] = kwargs.get("pk_set", set())
+    action: str = kwargs.get("action", "")
+    if action != "post_add":
+        return
+    followed_by = instance
+    for pk in pk_set:
+        following_user = User.objects.get(pk=pk)
+        follow = Follow.objects.filter(
+            followed_by=followed_by, following_to=following_user
+        ).first()
+        if not follow:
+            continue
+
+        notification = Notification()
+        notification.user = following_user
+        notification.from_user = followed_by
+        notification.followed_user = follow
+        notification.save()
+
+
+# @receiver(post_save, sender=Follow)
+# def on_following_created(sender, instance: Follow, **kwargs):
+
+#     print(sender)
+#     print(instance, type(instance))
+#     print(kwargs)
+#     noti = Notification()
+#     noti.user = instance.following_to
+#     noti.from_user = instance.followed_by
+#     noti.followed_user = instance
+#     noti.save()
