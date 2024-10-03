58dfabddec9d485c2a77d0ded083001d82774866
farrar142 <gksdjf1690@gmail.com>
2024-10-04 05:20:29 +0900

    fix:문법에러수정

--- base/settings/__init__.py
+++ base/settings/__init__.py
@@ -174,7 +174,7 @@ CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 CELERY_ACCEPT_CONTENT = ["pickle", "json"]
 CELERY_TASK_SERIALIZER = "pickle"
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000",,'https://cotton.honeycombpizza.link']
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://\w+\.honeycombpizza\.link$"]
 
 # S3
