846aca1c367fdb2a7c30ea64d18bfedf82664810
farrar142 <gksdjf1690@gmail.com>
2024-10-05 16:19:40 +0900

    fix:웹소켓수정

--- chats/consumers.py
+++ chats/consumers.py
@@ -23,37 +23,6 @@ class UserConsumer(AsyncJsonWebsocketConsumer):
         if self.channel_layer:
             await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    async def emit_message(self, event):
-        message = event["message"]
-        await self.send(text_data=json.dumps(message))
-
-    @classmethod
-    def send_message(cls, user_id: int | str, message: dict):
-        layer = get_channel_layer()
-        if not layer:
-            return
-        async_to_sync(layer.group_send)(
-            cls.get_group_name(user_id),
-            dict(type="emit_message", message=message),
-        )
-
-
-class ChatConsumer(AsyncJsonWebsocketConsumer):
-    @staticmethod
-    def get_group_name(group_id: int | str):
-        return f"message_group-{group_id}"
-
-    async def connect(self):
-        self.group_id = self.scope["url_route"]["kwargs"]["group_id"]
-        self.group_name = self.get_group_name(self.group_id)
-        if self.channel_layer:
-            await self.channel_layer.group_add(self.group_name, self.channel_name)
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        if self.channel_layer:
-            await self.channel_layer.group_discard(self.group_name, self.channel_name)
-
     async def emit_event(self, event):
         data = event["data"]
         await self.send(text_data=json.dumps(data))
--- chats/routings.py
+++ chats/routings.py
@@ -1,9 +1,8 @@
 from django.urls import re_path
 
-from .consumers import ChatConsumer, UserConsumer
+from .consumers import UserConsumer
 
 
 websocket_urlpatterns = [
-    re_path(r"ws/message_groups/(?P<group_id>\w+)/$", ChatConsumer.as_asgi()),
     re_path(r"ws/users/(?P<user_id>\w+)/$", UserConsumer.as_asgi()),
 ]
