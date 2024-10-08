e9aa21ca0e0830b23d16f73106553728207c32cf
farrar142 <gksdjf1690@gmail.com>
2024-10-08 14:05:44 +0900

    fix:쿼리에러수정

--- users/services/__init__.py
+++ users/services/__init__.py
@@ -72,11 +72,13 @@ class AuthService:
     @classmethod
     def signin_kakao(cls, code: str, redirect_uri: str):
         resp = cls.get_kakao_user(code, redirect_uri)
+        print(resp)
         kakao_id, nickname = resp["id"] = resp["kakao_account"]["profile"]["nickname"]
         temp_email = kakao_id + "@" + "kakao.com"
+        print(kakao_id, nickname)
         if not (
             user := User.objects.filter(
-                third_party_integrations__email=temp_email
+                third_party_integrations__provider_email=temp_email
             ).first()
         ):
             user = User(
