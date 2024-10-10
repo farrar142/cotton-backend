69ebd4d8864ab5dbe3b7f5ba346de4c90a259aea
farrar142 <gksdjf1690@gmail.com>
2024-10-10 18:18:16 +0900

    feature: NewsCralwer 모델추가

--- ai/models.py
+++ ai/models.py
@@ -1,8 +1,19 @@
+import urllib.parse
 from django.db import models
 from users.models import User
 
 
 # Create your models here.
+
+
+class NewsCralwer(models.Model):
+    collection_name = models.TextField(default="huffington")
+    news_url = models.URLField(default="https://huffpost.com")
+    url_icontains = models.CharField(max_length=255, default="/entry/")
+    article_tag = models.CharField(max_length=63, default="main")
+    article_id = models.CharField(max_length=63, default="main")
+
+
 class ChatBot(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="chatbots")
     character = models.TextField(default="")
