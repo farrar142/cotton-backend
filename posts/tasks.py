ef8a8a2409913b483f264e859aeb954f5fc74279
farrar142 <gksdjf1690@gmail.com>
2024-10-13 05:38:51 +0900

    fix:post의 child모델이 삭제되면 그에 상응하는 가중치를 추천포스트에 적용

--- posts/tasks.py
+++ posts/tasks.py
@@ -1,8 +1,21 @@
 from django.apps import apps
 from django.utils.timezone import localtime, timedelta
+from django.db import models
 from commons.caches import LRUCache, TimeoutCache
 from commons.celery import shared_task
 from commons.lock import get_redis
+
+
+def get_weights(instance):
+    from .models import View, Favorite, Bookmark, Repost
+
+    if isinstance(instance, Favorite):
+        weights = 5
+    elif isinstance(instance, Bookmark):
+        weights = 2
+    elif isinstance(instance, Repost):
+        weights = 10
+    return 1
 
 
 @shared_task()
@@ -12,21 +25,31 @@ def create_child_model(model_path: str, child_str: str, instance_id: int, user_i
     instance = Model.objects.filter(pk=instance_id).first()
     if not instance:
         return
-    manager = getattr(instance, child_str)
+
+    manager: models.Manager[models.Model] = getattr(instance, child_str)
     if manager.filter(user_id=user_id).exists():
         return
 
-    instance = getattr(instance, child_str).create(user_id=user_id)
-    from .models import View, Favorite, Bookmark, Repost
-
-    weights = 1
-    if isinstance(instance, Favorite):
-        weights = 5
-    elif isinstance(instance, Bookmark):
-        weights = 2
-    elif isinstance(instance, Repost):
-        weights = 10
+    instance = manager.create(user_id=user_id)
+    weights = get_weights(instance)
     push_recommended_list.delay(instance_id, weights)
+
+
+@shared_task()
+def delete_child_models(
+    model_path: str, child_str: str, instance_id: int, user_id: int
+):
+    path_splitted = model_path.split(".")
+    Model = apps.get_model(path_splitted[0], path_splitted[1])
+    instance = Model.objects.filter(pk=instance_id).first()
+    if not instance:
+        return
+    manager: models.Manager[models.Model] = getattr(instance, child_str)
+    if not (child := manager.filter(user_id=user_id).first()):
+        return
+    child.delete()
+    weights = get_weights(instance)
+    push_recommended_list.delay(instance_id, -weights)
 
 
 @shared_task()
